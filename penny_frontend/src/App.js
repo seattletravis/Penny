8608f71ed67b0c507e572ec421d6599a638002af
Travis Lamberte <travis@lamberte.com>
2024-06-16 11:34:20 -0700

    completely blank react project ready for moldinggit status!

--- penny_frontend/src/App.js
+++ penny_frontend/src/App.js
@@ -5,6 +5,10 @@ function App() {
 	return (
 		<div>
 			<Header />
+			<div>
+				This is the Penny Pressing Machine Adventure App - Welcome and happy
+				penny pressing!!!
+			</div>
 			<Footer />
 		</div>
 	);
